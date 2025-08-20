cf8174d841c68ede8b1307802c2c586e7b7bc3ab
diegobckn <diegobckn@users.noreply.github.com>
2025-08-20 16:37:20 -0300

    act 249

--- src/Componentes/ScreenDialog/PreventaDetalle.jsx
+++ src/Componentes/ScreenDialog/PreventaDetalle.jsx
@@ -15,13 +15,17 @@ import {
   Table,
   TableBody,
   TableRow,
-  TableCell
+  TableCell,
+  Paper,
+  TableHead
 } from "@mui/material";
 import SmallButton from "../Elements/SmallButton";
 import ModelConfig from "../../Models/ModelConfig";
 import System from "../../Helpers/System";
 import { textAlign, textTransform } from "@mui/system";
 import PreVenta from "../../Models/PreVenta";
+import { SelectedOptionsContext } from "../Context/SelectedOptionsProvider";
+
 
 var prods = [];
 for (let index = 1; index <= 5; index++) {
@@ -36,20 +40,33 @@ const PreventaDetalle = ({
 }) => {
 
 
+
+  const {
+    userData,
+    showMessage,
+    showAlert,
+    showConfirm,
+    showLoading,
+    hideLoading
+  } = useContext(SelectedOptionsContext);
+
+  const [detalles, setDetalles] = useState(null)
+
   useEffect(() => {
-    // if (info) {
-    //   PreVenta.findPreVenta({
-    //     "preVentaID": info.preVentaID,
-    //     "idCabecera": 0,
-    //     "folio": 0
-    //   }, (preventaServer) => {
-    //     console.log("info preventaServer", preventaServer)
-    //   }, (err) => {
-    //     alert(err)
-    //   })
+    if (info) {
+      PreVenta.findPreVenta({
+        "preVentaID": info.preVentaID,
+        "idCabecera": 0,
+        "folio": 0
+      }, (preventaServer) => {
+        console.log("info preventaServer", preventaServer)
+        setDetalles(preventaServer)
+      }, (err) => {
+        alert(err)
+      })
 
 
-    // }
+    }
   }, [openDialog])
 
   return openDialog && info && (
@@ -62,8 +79,71 @@ const PreventaDetalle = ({
       <DialogTitle>Detalles</DialogTitle>
       <DialogContent>
 
-        <Typography>Detalles de preventa</Typography>
 
+        {detalles && (
+          <>
+            Datos de Productos
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow sx={{ backgroundColor: "#2E3030D1" }}>
+                    <TableCell sx={{ color: "white" }}>Código Producto</TableCell>
+                    <TableCell sx={{ color: "white" }}>Descripción</TableCell>
+                    <TableCell sx={{ color: "white" }}>Precio Unidad</TableCell>
+                    <TableCell sx={{ color: "white" }}>Cantidad</TableCell>
+                    <TableCell sx={{ color: "white" }}>Costo</TableCell>
+                    <TableCell sx={{ color: "white" }}>Monto</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {detalles.map(
+                    (detalle, index) => {
+                      if (detalle.descripcion.toLowerCase() != "redondeo")
+                        return (
+                          <TableRow key={index}>
+                            <TableCell>{detalle.codProducto}</TableCell>
+                            <TableCell>{detalle.descripcion}</TableCell>
+                            <TableCell>
+                              {detalle.precioUnidad.toLocaleString("es-CL")}
+                            </TableCell>
+                            <TableCell>
+                              {detalle.cantidad.toLocaleString("es-CL")}
+                            </TableCell>
+                            <TableCell>
+                              {detalle.costo.toLocaleString("es-CL")}
+                            </TableCell>
+                            <TableCell>
+                              {(detalle.precioUnidad * detalle.cantidad).toLocaleString("es-CL")}
+                            </TableCell>
+                          </TableRow>
+                        )
+                      else
+                        return (
+                          <TableRow key={index}>
+                            <TableCell>{' '}</TableCell>
+                            <TableCell>{detalle.descripcion}</TableCell>
+                            <TableCell>
+                              {' '}
+                            </TableCell>
+                            <TableCell>
+                              {' '}
+                            </TableCell>
+                            <TableCell>
+                              {' '}
+                            </TableCell>
+                            <TableCell sx={{
+                            }}>
+                              {(detalle.precioUnidad * detalle.cantidad).toLocaleString("es-CL")}
+                            </TableCell>
+                          </TableRow>
+                        )
+                    })
+                  }
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        )}
 
       </DialogContent>
       <DialogActions>
--- src/definitions/Constants.ts
+++ src/definitions/Constants.ts
@@ -1,6 +1,6 @@
 const CONSTANTS = {
     appName: "EasyPOSLite",
-    appVersion: "1.0.248",
+    appVersion: "1.0.249",
     UNIDADES: [
         { idUnidad: 0, descripcion: "Sin unidad" },
         { idUnidad: 1, descripcion: "UNI" },
